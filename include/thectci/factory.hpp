f5aea47905fb204dca3f738bd5af0b649b38dc6d
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-24 14:00:21 +0200

    add factory

---
+++ include/thectci/factory.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <thectci/id.hpp>
+#include <memory>
+#include <cassert>
+#include <functional>
+#include <unordered_map>
+namespace the
+{
+namespace ctci
+{
+
+template < typename Base >
+class Creator
+{
+  public:
+    typedef std::unique_ptr< Base > base_pointer;
+    virtual ~Creator() {}
+
+    virtual base_pointer create() = 0;
+};
+
+template < typename Base, typename Child >
+class ExactCreator : public Creator< Base >
+{
+  public:
+    typedef std::unique_ptr< Base > base_pointer;
+
+    base_pointer create() override
+    {
+      return std::unique_ptr< Base >( new Child() );
+    }
+};
+
+
+template < typename Base >
+class Factory
+{
+  public:
+    typedef std::unique_ptr< Base > base_pointer;
+    typedef Creator< Base > BaseCreator;
+
+    void register_creator( Id class_id, BaseCreator& base_creator )
+    {
+      assert( m_creators.find( class_id ) == end( m_creators ) );
+      m_creators.emplace( std::make_pair( class_id, std::ref( base_creator ) ) );
+    }
+
+    base_pointer create( Id class_id ) const
+    {
+      typename Creators::const_iterator creator_iterator( m_creators.find( class_id ) );
+      if ( creator_iterator == end( m_creators ) )
+      {
+        return base_pointer( nullptr );
+      }
+
+      return creator_iterator->second.get().create();
+    }
+
+  private:
+    typedef std::reference_wrapper< BaseCreator > CreatorReference;
+    typedef std::unordered_map< Id, CreatorReference > Creators;
+    Creators m_creators;
+};
+
+}
+}
+
---
+++ test/test_factory.cpp
@@ -0,0 +1,78 @@
+#include <thectci/factory.hpp>
+
+#include <igloo/igloo_alt.h>
+using namespace igloo;
+
+namespace
+{
+  class foo
+  {
+    public:
+      add_ctci( "foo" );
+      virtual ~foo() {}
+
+      virtual const std::string name() const
+      {
+        return "foo";
+      }
+  };
+
+  class foo_child : public foo
+  {
+    public:
+      add_ctci( "foo child" );
+      virtual const std::string name() const override
+      {
+        return "foo child";
+      }
+  };
+
+  class second_foo_child : public foo
+  {
+    public:
+      add_ctci( "second foo child" );
+  };
+
+}
+
+Describe( an_exact_creator )
+{
+  It( creates_objects_referenced_by_base_type )
+  {
+    the::ctci::ExactCreator< foo, foo_child > creator;
+    std::unique_ptr< foo > foochild_instance( creator.create() );
+    AssertThat( foochild_instance->name(), Equals( "foo child" ) );
+  }
+
+};
+
+Describe( a_factory )
+{
+  void SetUp()
+  {
+    test_factory.reset( new TestFactory() );
+    test_factory->register_creator( foo_child::id, foochild_creator );
+    test_factory->register_creator( foo::id, foo_creator );
+  }
+
+  It( creates_objects_by_class_id )
+  {
+    std::unique_ptr< foo > foochild_instance( test_factory->create( foo_child::id ) );
+    AssertThat( foochild_instance->name(), Equals( "foo child" ) );
+
+    std::unique_ptr< foo > foo_instance( test_factory->create( foo::id ) );
+    AssertThat( foo_instance->name(), Equals( "foo" ) );
+  }
+
+  It( returns_nullptr_if_creator_is_not_registered_for_the_class )
+  {
+    std::unique_ptr< foo > foo_instance( test_factory->create( second_foo_child::id ) );
+    AssertThat( foo_instance.get(), Equals( static_cast< foo* >( nullptr ) ) );
+  }
+
+  typedef the::ctci::Factory< foo > TestFactory;
+  std::unique_ptr< TestFactory > test_factory;
+  the::ctci::ExactCreator< foo, foo_child > foochild_creator;
+  the::ctci::ExactCreator< foo, foo > foo_creator;
+};
+
